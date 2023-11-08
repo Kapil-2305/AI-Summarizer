c2653b6d46feed1cd3f1c86e7bb8b1280a815623
Kapil Paliwal <paliwalkapil2004@gmail.com>
2023-11-08 23:54:14 +0530

    Add Redux Toolkit query for article summary and update store middleware.

--- src/services/store.js
+++ src/services/store.js
@@ -1,9 +1,10 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
+import { articleApi } from "./article";
+
 export const store = configureStore({
     reducer: {
-
+        [articleApi.reducerPath]: articleApi.reducer
     },
-    middleware: (getDefaultMiddleware)=>
-        getDefaultMiddleware().concat()
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
 })
